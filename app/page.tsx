f21dddcb1dc4afef0de9fb6cfba8a15ce9012c2d
aldodevv <aldodevox@gmail.com>
2025-12-31 09:52:30 +0700

    add glass ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
+"use client"
+import LiquidGlass from "liquid-glass-react";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+    <div className="bg-[linear-gradient(rgba(0,0,0,.6),rgba(0,0,0,.6))] flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 dark:bg-black sm:items-start">
+        <LiquidGlass>
+          <div className="p-2">
+            <h2>Your content here</h2>
+            <p>This will have the liquid glass effect</p>
+          </div>
+        </LiquidGlass>
         <Image
           className="dark:invert"
           src="/next.svg"
@@ -12,7 +20,7 @@ export default function Home() {
           height={20}
           priority
         />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
+        <div className="bg-white flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
             To get started, edit the page.tsx file.
           </h1>
